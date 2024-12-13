f7793d56d92177fe2fd48fe4aec0e87924dd9595
Niranjan <146113809+niranjan5185@users.noreply.github.com>
2024-12-13 19:16:13 +0530

    Add files via upload

---
+++ Gas Agency System/firebase-config.js
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAGq8vsxtJMsj5T5_99mAkmZVl9GoaWAfU",
+  authDomain: "gas-agency-system-656b1.firebaseapp.com",
+  projectId: "gas-agency-system-656b1",
+  storageBucket: "gas-agency-system-656b1.firebasestorage.app",
+  messagingSenderId: "316213778703",
+  appId: "1:316213778703:web:a26af868c61279087e867a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
